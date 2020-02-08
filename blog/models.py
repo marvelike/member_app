35c5ffa281ea9d0d355e9f3cce3b91b6da55e74b
marvelike <marvelike7@gmail.com>
2020-02-08 19:41:41 +0100

    modification

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import models as model
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Posts(models.Model):
@@ -11,3 +12,17 @@ class Posts(models.Model):
 
 	def __str__(self):
 		return self.post_title + '_' + self.post_author
+
+
+class Users(AbstractUser):
+	first_name = models.CharField(max_length=250)
+	last_name = models.CharField(max_length=250)
+	email_address = models.EmailField()
+	username = models.CharField(unique=True)
+	profile_picture = models.ImageField()
+	sex_choices = ((MALE, 'Male'), (FEMALE, 'Female'),)
+	sex = models.CharField(choices = sex_choices, default=MALE)
+	bio = models.TextField()
+
+	def __str__(self):
+	 return self.first_name, self.last_name
